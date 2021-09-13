a0b8f9eb14815c87b884afb646ae4ea6c48e133b
Eliezer Couoh <eliezer.couoh@adira.com.mx>
2021-09-12 21:01:04 -0500

    animation icon

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -36,7 +36,7 @@ export default () =>(
                 <div className="sm:px-10">
                     <a href={cvPDF} download class="bg-color-primary hover:bg-blue-700 font-bold py-2 my-6 px-4 rounded inline-flex items-center">
                         <span className="text-white">Download CV  </span>
-                        <svg class="fill-current w-6 h-6 ml-2"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <svg class="fill-current w-6 h-6 ml-2 animate-bounce"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path fill="#FFFFFF" d="M8.29,13.29a1,1,0,0,0,0,1.42l3,3a1,1,0,0,0,1.42,0l3-3a1,1,0,0,0-1.42-1.42L13,14.59V3a1,1,0,0,0-2,0V14.59l-1.29-1.3A1,1,0,0,0,8.29,13.29ZM18,9H16a1,1,0,0,0,0,2h2a1,1,0,0,1,1,1v7a1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H8A1,1,0,0,0,8,9H6a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V12A3,3,0,0,0,18,9Z"/>
                         </svg>
                     </a>
