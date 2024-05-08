3b8adedae5ce3615d0e3ef823ce5cfc05047827d
eliezfer <eliezer.couoh.balam@gmail.com>
2024-05-07 23:12:57 -0600

    improve: about me

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,40 +1,43 @@
 import React from "react";
-import illustration from '../imgs/me.jpg';
-import cvPDF from '../assets/Eliezer_Couoh_CV-Mar-2022-EN.pdf'
+import illustration from '../imgs/me_2024.jpg';
+import cvPDF from '../assets/Eliezer_Couoh_Resume-2024-EN.pdf';
 
+const currentDate = new Date();
+const startingDate = new Date(2020, 0, 1); // 1 de enero de 2020
+const differenceInYears = currentDate.getFullYear() - startingDate.getFullYear();
+const formattedYears = String(differenceInYears).padStart(2, '0');
 
 export default () =>(
     <div id='aboutMe' className="container-center pt-20 sm:px-12 mx-auto">
         <h1 className=" font-bold text-3xl flex justify-center items-center " >About me</h1>
-        <span className="font-bold bg-gray-900 text-1xl txt-color-primary flex justify-center items-center w-48 mx-auto">Quién es Eliezfer?</span>
+        <span className="font-bold bg-gray-900 text-1xl txt-color-primary flex justify-center items-center w-48 mx-auto">Who is Eliezfer?</span>
         <div className=" py-10 flex flex-col md:flex-row justify-center items-center ">
             
             <img className=" w-56 object-contain rounded-xl mx-auto md:w-4/12 lg:w-2/6 xl:mx-auto" src={illustration} alt=""></img>
-            <div className=" pt-10 md:w-3/5 flex flex-col items-center ">
-               <p className="  px-10 text-justify">
-                Mi nombre es Eliezer Fernando Couoh Balam, de Mérida Yucatán. Actualmente (2021) he egresado de la carrera Ingeniería de Software de la de la Facultad de Matemáticas de la UADY.
-                Me he desarrollado en el área de desarrollo web, QA y DevOps en los últimos dos años y pretendo crecer en estas tres áreas para posicionarme como FullStack, para ello me considero una persona 
-                autodidacta y que mantiene un constante aprendizaje en diversas plataformas.
-                </p> 
+            <div className=" md:w-3/5 flex flex-col items-center ">
+             
+                <div className="px-10 sm:px-10 py-6 grid gap-x-2 grid-flow-col grid-cols-3 text-center ">
 
-                <div className="px-10 sm:px-10 py-6 grid gap-x-8 grid-flow-col grid-cols-3 text-center ">
+
                     <div className="flex flex-col justify-center items-center ">
-                        <span className=" bg-gray-900 text-white text-bold text-md sm:text-2xl"> 02+</span>
-                        <span className="text-sm sm:text-base"> Años <br></br>de experiencia  </span>
+                        <span className=" bg-gray-900 text-white text-bold text-md sm:text-2xl"> {formattedYears}+</span>
+                        <span className="text-sm sm:text-base"> Years <br></br>of experience  </span>
                     </div>
 
                     <div className="flex flex-col justify-center items-center text-center">
-                        <span className="bg-gray-900 text-white  text-bold text-md sm:text-2xl"> 02+ </span>
-                        <span className="text-sm sm:text-base"> Compañías <br></br> que he trabajado </span>
+                        <span className="bg-gray-900 text-white  text-bold text-md sm:text-2xl"> 03+ </span>
+                        <span className="text-sm sm:text-base"> Companies <br></br> I have worked for </span>
                     </div>
 
                     <div className="flex flex-col justify-center items-center text-center">
-                        <span className="bg-gray-900 text-white  text-bold text-md sm:text-2xl"> 07+ </span>
-                        <span className="text-sm sm:text-base"> Proyectos <br></br> que he colaborado</span>
+                        <span className="bg-gray-900 text-white  text-bold text-md sm:text-2xl"> 08+ </span>
+                        <span className="text-sm sm:text-base"> Projects <br></br> in which I've collaborated</span>
                     </div>
             
                 </div>
-                
+                <p className="  px-10 text-justify">
+                Dedicated software developer with a focus on web development, quality assurance, and DevOps. Experienced in backend development, specializing in creating RESTful APIs, implementing testing techniques such as TDD (Test-Driven Development) and unit testing, and establishing continuous integration and continuous deployment pipelines for applications.
+               </p> 
                 <div className="sm:px-10">
                     <a href={cvPDF} download class="bg-color-primary hover:bg-blue-700 font-bold py-2 my-6 px-4 rounded inline-flex items-center">
                         <span className="text-white">Download CV  </span>
